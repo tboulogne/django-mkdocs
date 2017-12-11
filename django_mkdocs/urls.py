988fd0d32d22d3dd84285b674eaa6fea5fc8abbc
tdball <trevor.d.ball@gmail.com>
2017-12-11 16:49:01 -0600

    no need to convert string to string, whoops

--- django_mkdocs/urls.py
+++ django_mkdocs/urls.py
@@ -7,5 +7,5 @@ admin.autodiscover()
 app_name = 'mkdocs'
 
 urlpatterns = [
-    path('<string:path>', views.documentation, name="mkdocs"),
+    path('<path>', views.documentation, name="mkdocs"),
 ]
